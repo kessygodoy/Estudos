05362a21ef8fc1ed2740b957c3d428d37ba993df
kessygodoy <kessyon@gmail.com>
2025-09-19 16:57:26 -0300

    consumindo hook cuystom

--- Udemy/HORA_DE_CODAR/9_CONTEXT_API/context_api/src/pages/Home/index.jsx
+++ Udemy/HORA_DE_CODAR/9_CONTEXT_API/context_api/src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
-import {useContext} from 'react'
-import {CounterContext} from '../../context/CounterContext'
+// import {useContext} from 'react'
+// import {CounterContext} from '../../context/CounterContext'
+
+// 4 - refatorando com hook
+import { useCounterContext } from '../../hooks/useCounterContext'
 
 import styles from './styles.module.css'
 import ChangeCounter from '../../components/ChangeCounter'
 
 const Home = () => {
-    const {counter} = useContext(CounterContext)
+    // const {counter} = useContext(CounterContext)
+    const {counter, setCounter} = useCounterContext()
+
   return (
     <div className={styles.container}>
       HOME
