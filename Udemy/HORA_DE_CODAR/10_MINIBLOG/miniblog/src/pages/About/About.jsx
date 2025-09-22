80f7cbb0d63248c8da92428a34c4901045bdb498
kessygodoy <kessyon@gmail.com>
2025-09-21 23:02:10 -0300

    Instalado e configurado o react routes

---
+++ Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hoome</h1>
+    </div>
+  )
+}
+
+export default Home
