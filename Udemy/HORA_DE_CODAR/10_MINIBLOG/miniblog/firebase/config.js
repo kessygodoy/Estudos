d5d7783161023b9ae6896ab05d50be158d8c0dc6
kessygodoy <kessyon@gmail.com>
2025-09-21 22:40:23 -0300

    criado projeto miniblog e criado o arquivo config do firebase

---
+++ Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbr41iA3rXhlL_XLuQ2pvIfJzHiIRMLWk",
+  authDomain: "miniblog-5037a.firebaseapp.com",
+  projectId: "miniblog-5037a",
+  storageBucket: "miniblog-5037a.firebasestorage.app",
+  messagingSenderId: "8908814270",
+  appId: "1:8908814270:web:09d87c7db8e7f573f6abef"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
