975065bcb4ffa9374dc797967e6ed47b88e2a36e
kessygodoy <kessyon@gmail.com>
2025-09-23 12:49:48 -0300

    configurado fonte e estilos no projeto

--- Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/App.jsx
+++ Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/App.jsx
@@ -3,18 +3,22 @@ import './App.css'
 import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   )
---
+++ Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Footer</h3>
+      <p>Mini Blog &copy; 2025</p>
+    </footer>
+  )
+}
+
+export default Footer
