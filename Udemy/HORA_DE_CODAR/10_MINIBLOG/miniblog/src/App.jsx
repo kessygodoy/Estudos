a8e38ee040d6aabb410e093136589c0b6974215d
kessygodoy <kessyon@gmail.com>
2025-09-25 09:59:45 -0300

    criado contexto de antenticação

--- Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/App.jsx
+++ Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/App.jsx
@@ -1,31 +1,33 @@
-import './App.css'
+import "./App.css";
 
-import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar.jsx';
-import Register from './pages/Register/Register.jsx';
-import Login from './pages/Login/Login.jsx';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar.jsx";
+import Register from "./pages/Register/Register.jsx";
+import Login from "./pages/Login/Login.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 function App() {
-
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
