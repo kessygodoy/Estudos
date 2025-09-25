e3b005e556a08429b6250735e83adea9ab879634
kessygodoy <kessyon@gmail.com>
2025-09-25 16:27:44 -0300

    Passado user para o provider

--- Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/App.jsx
+++ Udemy/HORA_DE_CODAR/10_MINIBLOG/miniblog/src/App.jsx
@@ -1,18 +1,44 @@
 import "./App.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar.jsx";
 import Register from "./pages/Register/Register.jsx";
 import Login from "./pages/Login/Login.jsx";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {onAuthStateChanged} from "firebase/auth"
+
+//hooks
+import {useState, useEffect} from "react"
+import { useAuthentication } from "./hooks/useAuthentication.jsx";
+
+//context
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 function App() {
+
+  const [user, setUser] = useState(undefined);
+  const {auth} = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  }, [auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{user}}>
         <BrowserRouter>
           <Navbar />
           <div className="container">
