ebcf70e73d70c3c6caef4989b4319eedb9856558
kessygodoy <kessyon@gmail.com>
2025-09-29 19:46:08 -0300

    Criando componente Header

--- Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(public)/page.tsx
+++ Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(public)/page.tsx
@@ -1,20 +1,11 @@
-"use client"
 
 import { useState } from "react";
-import "./page.css";
+import Header from "./_components/header";
 
 export default function Home(){
-  const [is, setIs] = useState(false);
   return (
-    <div className="Container">
-      <h2>A muié mais linda do mundo:</h2>
-      
-    { !is && <>
-      <button onClick={() => setIs(!is)}>Cliqe AQUI descobrir</button>
-    </>}
-      {is && (<><h1>Caroline Limoni </h1>
-      <h4>❤️</h4></>)}
-      
+    <div>
+      <Header />
     </div>
   )
 }
