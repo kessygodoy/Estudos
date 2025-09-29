797369e06365422cfdc8da1e394e1ae2e5b05542
kessygodoy <kessyon@gmail.com>
2025-09-29 18:27:43 -0300

    App criado

--- Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/page.tsx
+++ Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/page.tsx
@@ -1,7 +1,11 @@
+import "./page.css";
 export default function Home(){
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Container">
+      <h2>A muié mais linda do mundo:</h2>
+      
+      <h1>Caroline Limoni </h1>
+      <h4>❤️</h4>
     </div>
   )
 }
