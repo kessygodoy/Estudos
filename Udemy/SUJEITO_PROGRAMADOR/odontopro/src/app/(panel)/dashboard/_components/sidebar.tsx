09f34fbffc7eab93e7f345d7ffbfd7d7375ecaf9
kessygodoy <kessyon@gmail.com>
2025-10-02 17:48:09 -0300

    sidebar mobile

--- Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/_components/sidebar.tsx
+++ Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/_components/sidebar.tsx
@@ -11,7 +11,8 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button";
-import { List } from "lucide-react";
+import { Banknote, CalendarDays, Folder, List, Settings } from "lucide-react";
+import Link from "next/link";
 
 export function SidebarDashboard({ children }: { children: React.ReactNode }) {
 
@@ -26,7 +27,9 @@ export function SidebarDashboard({ children }: { children: React.ReactNode }) {
                 "md:ml-20": isCollapsed,
                 "md:ml-64": !isCollapsed
             })}>
-                <header className="md:hidden">
+                <header className="md:hidden flex items-center justify-between border-b px-2 md:px-6 py-4 z-10
+                sticky top-0 bg-white
+                ">
                     <Sheet>
                         <div className="flex items-center gap-4">
                             <SheetTrigger asChild>
@@ -40,7 +43,45 @@ export function SidebarDashboard({ children }: { children: React.ReactNode }) {
                                 Menu OdontoPRO
                             </h1>
                         </div>
+                        <SheetContent side="left" className="sm:max-w-xs text-black">
+                        <SheetTitle>OdontoPro</SheetTitle>
+                        <SheetDescription>
+                            Menu administrativo
+                        </SheetDescription>
+
+                        <nav className="grid gap-2 text-base pt-5">
+                            <SidebarLink 
+                            href="/dashboard"
+                            label="Agendamentos"
+                            pathname={pathname}
+                            isCollapsed={isCollapsed}
+                            icon={<CalendarDays className="w-5 h-5" />}
+                            />
+                            <SidebarLink 
+                            href="/dashboard/services"
+                            label="Serviços"
+                            pathname={pathname}
+                            isCollapsed={isCollapsed}
+                            icon={<Folder className="w-5 h-5" />}
+                            />
+                            <SidebarLink 
+                            href="/dashboard/profile"
+                            label="Profile"
+                            pathname={pathname}
+                            isCollapsed={isCollapsed}
+                            icon={<Settings className="w-5 h-5" />}
+                            />
+                            <SidebarLink 
+                            href="/dashboard/plans"
+                            label="Planos"
+                            pathname={pathname}
+                            isCollapsed={isCollapsed}
+                            icon={<Banknote className="w-5 h-5" />}
+                            />
+                        </nav>
+                    </SheetContent>
                     </Sheet>
+                    
                 </header>
                 <main className="flex flex-1 py-4 px-2 md:p-6">
                     {children}
@@ -48,5 +89,30 @@ export function SidebarDashboard({ children }: { children: React.ReactNode }) {
             </div>
 
         </div>
+    )
+}
+
+interface SidebarLinkProps {
+    href: string;
+    icon: React.ReactNode;
+    label: string;
+    pathname: string;
+    isCollapsed: boolean
+}
+
+function SidebarLink( { href, icon, label, pathname, isCollapsed }: SidebarLinkProps
+ ) {
+    return(
+        <Link href={href}>
+            <div 
+                className={`flex items-center gap-2  px-3 py-2 rounded-md  transition-colors 
+                    ${ pathname === href && ( "bg-blue-500 text-white")}` }    
+                
+            >
+                <span className="w-6 h-6">{icon}</span>
+                {!isCollapsed && <span>{label}</span>}
+            </div>
+        </Link>
+        
     )
 }
--- Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/page.tsx
+++ Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/page.tsx
@@ -1,7 +1,15 @@
 export default function Dashboard(){
     return (
-        <div>
+        <div className="flex flex-2 flex-col">
             <h1>Página Dashboard</h1>
+
+            <div className="w-full h-[600px] bg-gray-700 mb-10"></div>
+
+            <div className="w-full h-[600px] bg-gray-500 mb-10"></div>
+
+            <div className="w-full h-[600px] bg-gray-700 mb-10"></div>
+
+            
         </div>
     )
 }
