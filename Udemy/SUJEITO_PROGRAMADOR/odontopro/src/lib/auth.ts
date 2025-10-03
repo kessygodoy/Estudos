b74f6a87871aef4c797c45cea3ae642b6f57ec21
kessygodoy <kessyon@gmail.com>
2025-10-03 16:47:37 -0300

    Criado banco de dados, instalado prisma orm, criado models e schema, gerando migrations, configurando authjs

---
+++ Udemy/SUJEITO_PROGRAMADOR/odontopro/src/lib/auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth"
+import prisma from "./prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    trustHost: true,
+    providers: [GitHub, Google],
+})
