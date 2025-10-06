94b1e7f6326c3d3e782b4a03f977efb594927958
kessygodoy <kessyon@gmail.com>
2025-10-06 18:39:48 -0300

    Criando pagina de perfil

--- Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/profile/page.tsx
+++ Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/profile/page.tsx
@@ -1,4 +1,17 @@
-export default function Profile(){
+import getSession from "@/lib/getSession"
+import { redirect } from "next/navigation";
+import { getUserData } from "./_data-access/get-info-user";
+
+export default async function Profile(){
+        const session = await getSession();
+    
+        if(!session) {
+            redirect("/")
+        }
+
+        const user = await getUserData({userId: session.user.id})
+        console.log("GetUserData: ", user)
+
     return(
         <section>
             <h1>Pagina perfil</h1>
