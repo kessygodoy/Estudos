e8c49bd58bea35ab6125ea128f389c2083df4ce2
kessygodoy <kessyon@gmail.com>
2025-10-15 12:38:24 -0300

    funções

---
+++ Udemy/devbook/4 - Funções/funcoes.go
@@ -0,0 +1,27 @@
+package main
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(10, 20)
+	println(soma)
+
+	var f = func(txt string) string {
+		println(txt)
+		return txt
+	}
+
+	resultado := f("Texto da função 1") + " - " + f("Texto da função 2")
+	println(resultado)
+
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	println(resultadoSoma, resultadoSubtracao)
+}
