1bdcfdcb8f08565b4e5c9ab29086f3b2ccdc6570
kessygodoy <kessyon@gmail.com>
2025-10-15 11:32:11 -0300

    Pacotes externos

--- Udemy/devbook/1 - Pacotes/go.mod
+++ Udemy/devbook/1 - Pacotes/go.mod
@@ -1,3 +1,5 @@
 module modulo
 
 go 1.25.3
+
+require github.com/badoux/checkmail v1.2.4 // indirect
--- Udemy/devbook/1 - Pacotes/main.go
+++ Udemy/devbook/1 - Pacotes/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "modulo/auxiliar"
+import (
+	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
+)
 
 func main() {
 	println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("kessyon@gmail.com")
+	println(erro)
 }
