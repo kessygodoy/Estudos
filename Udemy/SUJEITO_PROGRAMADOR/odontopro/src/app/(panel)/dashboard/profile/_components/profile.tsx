d1da96b2cffe03c6d027835d4bfceb0a2b44086d
kessygodoy <kessyon@gmail.com>
2025-10-15 10:45:25 -0300

    curso python

--- Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ Udemy/SUJEITO_PROGRAMADOR/odontopro/src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -1,7 +1,12 @@
 "use client"
 import { useProfileForm } from "./profile-form"
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function ProfileContent(){
+
+    const form = useProfileForm();
+
+  
     return(
         <div>
             <h1>TESTE</h1>
