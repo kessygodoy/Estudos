11e72c2b74fd213c698a71737bbd770f7d6cff90
kessy godoy <kessyon@gmail.com>
2025-12-18 19:51:42 -0300

    Feita p´ágina de favoritos.

--- Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao5/prime-filmes/src/pages/favoritos/index.js
+++ Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao5/prime-filmes/src/pages/favoritos/index.js
@@ -1,7 +1,20 @@
+import './index.css';
 export default function Favoritos() {
+    const filmes = localStorage.getItem("filmes");
+    const filmesParse = JSON.parse(filmes) || [];
     return (
-        <div>
-            <h1>Meus filmes favoritos</h1>
-        </div>
+        <>
+            <h1>  Meus filmes favoritos</h1>
+            <div className="container-fav">
+
+                {filmesParse.map((filme) => (
+                    <div key={filme.id} className="fav-item">
+                        <img className="backdrop-favs-img" src={`https://image.tmdb.org/t/p/w500/${filme.backdrop_path}`} alt={filme.title} />
+                        <p>Nota: {filme.vote_average} / 10</p>
+                        <strong>{filme.title}</strong>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
