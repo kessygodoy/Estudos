afdc935e4c62d83dfdd2c06b208e0fe83c3853a4
kessy godoy <kessyon@gmail.com>
2025-12-20 16:04:16 -0300

    Criada função de excluir filmes do favoritos.

--- Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao5/prime-filmes/src/pages/favoritos/index.js
+++ Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao5/prime-filmes/src/pages/favoritos/index.js
@@ -1,17 +1,37 @@
+import { Link } from 'react-router-dom';
 import './index.css';
+import { useEffect, useState } from 'react';
+
 export default function Favoritos() {
-    const filmes = localStorage.getItem("filmes");
-    const filmesParse = JSON.parse(filmes) || [];
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        function getFilmes() {
+            setFilmes(JSON.parse(localStorage.getItem("filmes")));
+
+        }
+
+        getFilmes()
+    }, []);
+
+    function apagarFilme(id) {
+        const filmesFiltrados = filmes.filter((filme) => filme.id != id)
+        localStorage.setItem("filmes", JSON.stringify(filmesFiltrados))
+        setFilmes(filmesFiltrados)
+    }
+
     return (
         <>
             <h1>  Meus filmes favoritos</h1>
             <div className="container-fav">
 
-                {filmesParse.map((filme) => (
+                {filmes.map((filme) => (
                     <div key={filme.id} className="fav-item">
                         <img className="backdrop-favs-img" src={`https://image.tmdb.org/t/p/w500/${filme.backdrop_path}`} alt={filme.title} />
                         <p>Nota: {filme.vote_average} / 10</p>
                         <strong>{filme.title}</strong>
+                        <button onClick={() => apagarFilme(filme.id)}>Remover</button>
+                        <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
                     </div>
                 ))}
             </div>
