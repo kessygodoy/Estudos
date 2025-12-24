9c1a28c185b356dff40311b7bfcdaa2a6e829fb0
kessy godoy <kessyon@gmail.com>
2025-12-24 13:36:37 -0300

    exemplos basicos de crud

--- Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao6/index.js
+++ Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao6/index.js
@@ -1,9 +1,47 @@
 const express = require("express");
 
-const server = express()
+const server = express();
 
-server.get('/', (req, res) => {
-    return res.json({ message: "Hello World" })
+// é necessário para que o express entenda o json
+server.use(express.json())
+
+const cursos = ['Node.js', 'React', 'React Native']
+
+server.get('/cursos', (req, res) => {
+    return res.json(cursos)
+})
+
+server.get('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json({ curso: `Aprendendo ${cursos[index]}` })
+})
+
+//criar
+server.post('/cursos', (req, res) => {
+    const { name } = req.body;
+    cursos.push(name)
+
+    return res.json(cursos)
+})
+
+//atualizar
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos)
+})
+
+//deletar
+server.delete('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    const deleted = cursos.splice(index, 1);
+
+    return res.send(deleted + " deletado.")
 })
 
 server.listen(3000)
