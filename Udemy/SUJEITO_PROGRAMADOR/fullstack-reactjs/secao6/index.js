1c9973490c47250ef32eb1dc88d882e5a1a06a6a
kessy godoy <kessyon@gmail.com>
2025-12-25 16:51:36 -0300

    feat: Add initial Express server with CRUD operations for courses, including global and local middlewares.

--- Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao6/index.js
+++ Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao6/index.js
@@ -7,22 +7,46 @@ server.use(express.json())
 
 const cursos = ['Node.js', 'React', 'React Native']
 
-server.use((req, res) => {
-    console.log("Recebemos uma requisição")
+//middleware global
+server.use((req, res, next) => {
+    console.log(`URL CHAMADA: ${req.url}`)
+
+    return next();
 })
+
+//middleware local para checar se o nome do curso existe
+function checkCurso(req, res, next) {
+    if (!req.body.name) {
+        return res.status(400).json({ error: "Nome do curso é obrigatório" })
+    }
+
+    return next();
+}
+
+function checkIndexCurso(req, res, next) {
+    const curso = cursos[req.params.index]
+
+    if (curso == undefined) {
+        return res.status(404).json({ error: "Curso não encontrado" })
+    }
+
+    req.curso = curso;
+
+    return next();
+}
 
 server.get('/cursos', (req, res) => {
     return res.json(cursos)
 })
 
-server.get('/cursos/:index', (req, res) => {
-    const { index } = req.params;
+server.get('/cursos/:index', checkIndexCurso, (req, res) => {
+    // const { index } = req.params;
 
-    return res.json({ curso: `Aprendendo ${cursos[index]}` })
+    return res.json(req.curso)
 })
 
 //criar
-server.post('/cursos', (req, res) => {
+server.post('/cursos', checkCurso, (req, res) => {
     const { name } = req.body;
     cursos.push(name)
 
@@ -30,17 +54,17 @@ server.post('/cursos', (req, res) => {
 })
 
 //atualizar
-server.put('/cursos/:index', (req, res) => {
+server.put('/cursos/:index', checkCurso, checkIndexCurso, (req, res) => {
     const { index } = req.params;
     const { name } = req.body;
 
     cursos[index] = name;
 
-    return res.json(cursos)
+    return res.json(req.curso)
 })
 
 //deletar
-server.delete('/cursos/:index', (req, res) => {
+server.delete('/cursos/:index', checkIndexCurso, (req, res) => {
     const { index } = req.params;
 
     const deleted = cursos.splice(index, 1);
