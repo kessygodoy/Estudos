e006d9d1b1948433d9cfe42250078d4835c89b83
kessy godoy <kessyon@gmail.com>
2026-01-05 07:49:27 -0300

    feat: implement initial backend setup including environment variables, dependencies, and a user authentication service.

--- Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao7/pizzaria/backend/src/services/user/AuthUserService.ts
+++ Udemy/SUJEITO_PROGRAMADOR/fullstack-reactjs/secao7/pizzaria/backend/src/services/user/AuthUserService.ts
@@ -1,3 +1,6 @@
+import { compare } from "bcryptjs";
+import prismaClient from "../../prisma";
+import { sign } from "jsonwebtoken";
 
 interface AuthUserServiceProps {
     email: string;
@@ -6,9 +9,39 @@ interface AuthUserServiceProps {
 
 class AuthUserService {
     async execute({ email, password }: AuthUserServiceProps) {
-        console.log({ email, password })
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
 
-        return "Logado!"
+        if (!user) {
+            throw new Error("Email/Senha invalidos")
+        }
+
+        const passwaordMatch = await compare(password, user.password)
+
+        if (!passwaordMatch) {
+            throw new Error("Email/Senha invalidos")
+        }
+
+        //gerar token jwt
+        const token = sign({
+            name: user.name,
+            email: user.email
+        }, process.env.JWT_SECRET as string, {
+            subject: user.id,
+            expiresIn: "30d"
+        })
+        
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token
+        }
     }
 }
 
