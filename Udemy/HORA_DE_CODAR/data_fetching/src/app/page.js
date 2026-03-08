a94e01931937d257be45ea88e4b666d598245a7c
kessygodoy <kessyon@gmail.com>
2026-03-08 10:28:03 -0300

    revalidate e force-dynamic cache

--- Udemy/HORA_DE_CODAR/data_fetching/src/app/page.js
+++ Udemy/HORA_DE_CODAR/data_fetching/src/app/page.js
@@ -1,24 +1,15 @@
 import { deleteTodo } from "@/actions";
 import Button from "@/components/Button";
 import { db } from "@/db";
-import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+
+export const revalidate = 20;
+//com esse export o next vai revalidar a pagina(cache) a cada 20 segundos
 
 export default async function Home() {
   // 3 - Resgatando dados do banco
   const todos = await db.todo.findMany()
 
-  // async function deleteTodo(formData) {
-  //   "use server"
-  //   const id = Number(formData.get("id"))
-  //   await db.todo.delete({
-  //     where: {
-  //       id: id
-  //     }
-  //   })
-  //   redirect("/")
-  // }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16  sm:items-start">
