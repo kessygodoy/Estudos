e7d47e17e779d3790addf9b77cc7664b6a3b00a5
kessygodoy <kessyon@gmail.com>
2026-03-14 10:17:21 -0300

    protegendo paginas com middleware

---
+++ Udemy/HORA_DE_CODAR/auth/app/middleware/page.tsx
@@ -0,0 +1,7 @@
+const MiddlewarePage = () => {
+    return <div>
+        <h1>Pagina protegida por middleware</h1>
+    </div>
+}
+
+export default MiddlewarePage;
--- Udemy/HORA_DE_CODAR/auth/app/page.tsx
+++ Udemy/HORA_DE_CODAR/auth/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Link className="text-blue-500 hover:text-blue-600" href="/client">Página de Cliente</Link>
         <Link className="text-blue-500 hover:text-blue-600" href="/server">Página de Servidor</Link>
+        <Link className="text-blue-500 hover:text-blue-600" href="/middleware">Página de com middleware</Link>
       </main>
     </div>
   );
