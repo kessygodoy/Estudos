d62fde9ff702f30f58c30c3ab7d9d3c1d67922d8
kessygodoy <kessyon@gmail.com>
2026-03-18 16:28:50 -0300

    criadas 3 primeiras rotas get

--- Udemy/JorgeSantAna/Code/app_super_gestao/routes/web.php
+++ Udemy/JorgeSantAna/Code/app_super_gestao/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "Olá seja bem vindo ao curso";
 });
+
+Route::get('/sobre-nos', function () {
+    return "Sobre nós";
+});
+
+Route::get('/contato', function () {
+    return "Contato";
+});
