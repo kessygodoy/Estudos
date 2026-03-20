bf1161af552a0051c805b8829fac7132cf9c50d6
kessygodoy <kessyon@gmail.com>
2026-03-20 17:17:02 -0300

    criado navegação apenas para estudos

--- Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/site/principal.blade.php
+++ Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/site/principal.blade.php
@@ -1,1 +1,7 @@
 <h3>Principal (VIEW)</h3>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/sobre-nos">Sobre Nós</a></li>
+    <li><a href="/contato">Contato</a></li>
+</ul>
--- Udemy/JorgeSantAna/Code/app_super_gestao/routes/web.php
+++ Udemy/JorgeSantAna/Code/app_super_gestao/routes/web.php
@@ -11,8 +11,3 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class , 'principal']
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class , 'sobreNos']);
 
 Route::get('/contato', [App\Http\Controllers\ContatoController::class , 'contato']);
-
-
-Route::get('/contato/{nome}/{categoria_id}', function (string $nome = "Desconhecido", int $categoria_id = 1) {
-    echo "Estamos aqui: $nome - $categoria_id";
-})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
