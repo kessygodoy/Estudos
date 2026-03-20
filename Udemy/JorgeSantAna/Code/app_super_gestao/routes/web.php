af6e16fbbf3b123ea84311cd60a2a29c1d54c854
kessygodoy <kessyon@gmail.com>
2026-03-20 16:46:01 -0300

    tratando parametros de rotas

--- Udemy/JorgeSantAna/Code/app_super_gestao/routes/web.php
+++ Udemy/JorgeSantAna/Code/app_super_gestao/routes/web.php
@@ -13,6 +13,6 @@ Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class , 'sob
 Route::get('/contato', [App\Http\Controllers\ContatoController::class , 'contato']);
 
 
-Route::get('/contato/{nome}/{categoria}/{assunto?}/{mensagem?}', function (string $nome, string $categoria, string $assunto = 'assunto não informado', string $mensagem = 'mensagem não informada') {
-    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
-});
+Route::get('/contato/{nome}/{categoria_id}', function (string $nome = "Desconhecido", int $categoria_id = 1) {
+    echo "Estamos aqui: $nome - $categoria_id";
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
