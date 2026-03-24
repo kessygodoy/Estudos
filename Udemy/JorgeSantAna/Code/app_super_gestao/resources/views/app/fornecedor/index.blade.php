1729ea6aecfa375cf7e158055b56615b2b868004
kessygodoy <kessyon@gmail.com>
2026-03-24 15:58:45 -0300

    while no blade

--- Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,43 +1,40 @@
 <h3>Fornecedor</h3>
 
 @php
-    
+
 @endphp
- 
+
 {{-- @dd($fornecedores) --}}
 
 @isset($fornecedores) <!-- verifica se a variavel existe -->
-@if(count($fornecedores) >0 && count($fornecedores) <=10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem varios fornecedores cadastrados</h3>
-@else
-    <h3>Nenhum fornecedor cadastrado</h3>
-@endif
+    @if(count($fornecedores) > 0 && count($fornecedores) <= 10)
+        <h3>Existem alguns fornecedores cadastrados</h3>
+    @elseif(count($fornecedores) > 10)
+        <h3>Existem varios fornecedores cadastrados</h3>
+    @else
+        <h3>Nenhum fornecedor cadastrado</h3>
+    @endif
 
 
-Fornecedor: {{ $fornecedores[1]['nome'] }}
-<br>
-Status:
-@if( !($fornecedores[1]['status'] == 'S'))
-    Inativo
-@else
-    Ativo
-@endif
-<br>
-@isset($fornecedores[0]['cnpj'])
-
-    @for($i = 0; $i < count($fornecedores); $i++)
-    CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido'}}
+    Fornecedor: {{ $fornecedores[1]['nome'] }}
     <br>
-    Telefone: {{ $fornecedores[$i]['ddd'] . ' - ' . $fornecedores[$i]['telefone'] ?? 'Dado não foi preenchido'}}
-    @endfor
-@endisset
+    Status:
+    @if(!($fornecedores[1]['status'] == 'S'))
+        Inativo
+    @else
+        Ativo
+    @endif
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        @php $i = 0 @endphp
+        @while(isset($fornecedores[$i]['nome']))
+            CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido'}}
+            <br>
+            Telefone: {{ $fornecedores[$i]['ddd'] . ' - ' . $fornecedores[$i]['telefone'] ?? 'Dado não foi preenchido'}}
+            <hr>
+            @php $i++ @endphp
+        @endwhile
+    @endisset
+    <br>
 
 @endisset
-
-<br>
-
-
-    {{ $i }}<br>
-  
