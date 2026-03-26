6beee8160729dc86f8c3a18bc8bea32b3fc9a120
kessygodoy <kessyon@gmail.com>
2026-03-26 11:07:37 -0300

    Enviando formulario de contato

--- Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/site/layouts/basico.blade.php
+++ Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/site/layouts/basico.blade.php
@@ -8,7 +8,7 @@
     </head>
 
     <body>
-        
+        @include('site.layouts._partials.topo')
         @yield('conteudo')
         
     </body>
--- Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/site/sobre-nos.blade.php
+++ Udemy/JorgeSantAna/Code/app_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -2,20 +2,6 @@
 
 @section('titulo', 'Sobre nós')
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="{{ asset('img/logo.png') }}">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.principal') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobre-nos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
 
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
